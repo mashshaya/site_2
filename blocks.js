971c96f0d2bb778e5b2a6ac1e9517b0b88df28ef
mashshaya <mashakova833@gmail.com>
2024-12-04 01:04:44 +0300

    Add files via upload

---
+++ blocks.js
@@ -0,0 +1,74 @@
+function sort(){
+    let price=document.getElementById("price")
+    let title=document.getElementById("title")
+    if(price.checked){
+        document.getElementById('node_for_insert').innerHTML = '';
+        getResponce()}
+
+    if(title.checked){
+        document.getElementById('node_for_insert').innerHTML = '';
+        getResponce1()}
+}
+
+async function getResponce() {
+
+    let responce = await fetch("blocks.json")
+
+    let content = await responce.text()
+    console.log(content)
+    content = JSON.parse(content)
+    content = content.splice(0, 8)
+    console.log(content)
+    let key
+    content_price=content.sort((a, b) => a.price - b.price);
+
+    let node_for_insert = document.getElementById("node_for_insert")
+    for (key in content_price) {
+                node_for_insert.innerHTML += `
+                <li style="width: 200px" class="d-flex flex-column m-1 p-1 border bg-body">
+                <img style="width: 150px" class="align-self-center" src=${content[key].img}>
+                <h5 class="card-title">${content[key].title}</h5>
+                <p class="card-text">${content[key].description} Цена: ${content[key].price} рублей</p>
+                <input type="hidden" name= "vendor_code" value=${content[key].vendor_code}>
+                <p class="card-text" >Заказать <input class="w-25" type="text" value="0" name="check"></p>
+                </li>
+                        `
+            }
+
+}
+async function getResponce1() {
+    let responce = await fetch("blocks.json")
+
+    let content = await responce.text()
+    console.log(content)
+    content = JSON.parse(content)
+    content = content.splice(0, 8)
+    console.log(content)
+    let key
+    content_title=content.sort((a, b) => {
+    const nameA = a.title.toUpperCase();
+    const nameB = b.title.toUpperCase();
+      if (nameA < nameB) {
+        return -1;
+      }
+      if (nameA > nameB) {
+        return 1;
+      }
+      return 0;
+    });
+    let node_for_insert = document.getElementById("node_for_insert")
+    for (key in content_title) {
+                node_for_insert.innerHTML += `
+                <li style="width: 200px" class="d-flex flex-column m-1 p-1 border bg-body">
+                <img style="width: 150px" class="align-self-center" src=${content[key].img}>
+                <h5 class="card-title">${content[key].title}</h5>
+                <p class="card-text">${content[key].description} Цена: ${content[key].price} рублей</p>
+                <input type="hidden" name= "vendor_code" value=${content[key].vendor_code}>
+                <p class="card-text" >Заказать <input class="w-25" type="checkbox" name="check" value="0" onClick='this.value = this.checked ? 1 : 0'></p>
+                </li>
+                        `
+            }
+
+}
+
+sort()
